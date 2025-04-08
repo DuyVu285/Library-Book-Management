cc63a502d62280159ca97136c24d85207ed2ee1a
Vũ Nhật Duy <vunhatduy285@gmail.com>
2025-04-08 13:02:34 +0700

    remove testpy

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgresql:password123@localhost/library_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password123@localhost/library_db")
 SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
